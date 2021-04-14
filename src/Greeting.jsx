a773bca11a99615aff59d27c29cd13027d6b9c5d
HarryFung <harryfung.k.h@outlook.com>
2021-04-14 23:37:34 +0800

    greeting

--- src/Greeting.jsx
+++ src/Greeting.jsx
@@ -8,6 +8,7 @@ function Greeting() {
     return (
         <div id="greeting" className="">
             <div className="background_minion"></div>
+            <h1 id="greeting_title">Hello from Harry Fung</h1>
             <Glass id="outer_circle" className="greeting_circle">
                 {/* <div id="outer_circle" className="greeting_circle"> */}
                 <Glass id="inner_circle" className="greeting_circle">
