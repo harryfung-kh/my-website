3df5b3917c93db6e2743f79753464754420f2fd7
harryfung-kh <harryfung.k.h@outlook.com>
2021-04-29 16:37:04 +0800

    Main Page and Navbar

--- src/Pages/MainPage/MainPage.jsx
+++ src/Pages/MainPage/MainPage.jsx
@@ -1,17 +1,15 @@
 import React from 'react';
 import './MainPage.css'
 import Header from './Header'
+
 function MainPage(){
     return(
         <div id="main" className="main_grid_container">
             <Header/>
             <main id="content"></main>
             <footer id="footer"></footer>
-
-
         </div>
     );
-
 }
 
 export default MainPage
