864cd8b388017a4b8a82d8171a1bb2b8bb2f96ec
harryfung-kh <harryfung.k.h@outlook.com>
2021-04-29 15:53:41 +0800

    header with items

--- src/Pages/MainPage/Header.jsx
+++ src/Pages/MainPage/Header.jsx
@@ -1,13 +1,21 @@
 import React from 'react';
 import './Header.css'
+import logo from '../../assets/logo.png';
 
 function Header() {
     return (
         <div id="header" className="header">
             <div className="header_container">
-                <div className="header_left"></div>
+                <div className="header_left">
+                    <div>Logo</div>
+                    {/* <img id="header_logo" src={logo} alt="Logo"></img> */}
+                </div>
                 <div></div>
-                <div className="header_right"></div>
+                <div className="header_right">
+                    <a id="header_home" className="header_link_current_page" href="#">Home</a>
+                    <a href="https://facebook.com.hk">Facebook</a>
+                    <a href="https://google.com.hk">Google</a>
+                </div>
             </div>
         </div>
     );
--- src/Pages/MainPage/MainPage.jsx
+++ src/Pages/MainPage/MainPage.jsx
@@ -3,11 +3,9 @@ import './MainPage.css'
 import Header from './Header'
 function MainPage(){
     return(
-        <div id="main" className="content_grid_container">
+        <div id="main" className="main_grid_container">
             <Header/>
-            <main id="content">
-
-            </main>
+            <main id="content"></main>
             <footer id="footer"></footer>
 
 
