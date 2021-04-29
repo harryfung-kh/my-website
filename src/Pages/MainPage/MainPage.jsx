579ed3690c04b6f95ae22b73720137bb044069c9
harryfung-kh <harryfung.k.h@outlook.com>
2021-04-29 09:53:37 +0800

    main pagea

---
+++ src/Pages/MainPage/MainPage.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './MainPage.css'
+
+function Home(){
+    return(
+        <div id="main" className="content_grid_container">
+            <div id="header"> 
+
+            </div>
+            <main id="content">
+
+            </main>
+            <footer id="footer"></footer>
+
+
+        </div>
+    );
+
+}
