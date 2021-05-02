8401698b8a08b1156e806d7f6c06b30c95d76885
HarryFung <harryfung.k.h@outlook.com>
2021-05-03 02:46:16 +0800

    footer test

--- src/Pages/MainPage/Footer.jsx
+++ src/Pages/MainPage/Footer.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import './Footer.css'
+import emailLight from '../../assets/email-light.png'
 
 function Footer() {
     return (
         <footer id="footer">
             <div class="footer_container">
-                <div class="footer_left"></div>
+                <div class="footer_left">
+                    <span>tetst</span>
+                    <span>tetst</span>
+                    <span>tetst</span>
+                    <span>tetst</span>
+                </div>
                 <div class="footer_right"></div>
             </div>
         </footer>
