a1b29f024da159a9d182abfce17a7c41bf46ecb2
HarryFung <harryfung.k.h@outlook.com>
2021-05-02 23:11:54 +0800

    Footer

---
+++ src/Pages/MainPage/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Footer.css'
+
+function Footer() {
+    return (
+        <footer id="footer">
+            Hello I am a footer
+        </footer>
+    )
+}
+export default Footer;
