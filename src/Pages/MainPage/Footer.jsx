6ef319bc14d2df97167517aa58a9a45ace0a83b2
harryfung-kh <harryfung.k.h@outlook.com>
2021-05-06 12:00:27 +0800

    Footer and related resource

--- src/Pages/MainPage/Footer.jsx
+++ src/Pages/MainPage/Footer.jsx
@@ -5,14 +5,29 @@ import emailLight from '../../assets/email-light.png'
 function Footer() {
     return (
         <footer id="footer">
-            <div class="footer_container">
-                <div class="footer_left">
-                    <span>tetst</span>
-                    <span>tetst</span>
-                    <span>tetst</span>
-                    <span>tetst</span>
+            <div id="animation" class="">
+                <div id="footer_icon"></div>
+                <h3>TEMPLATE</h3>
+                <p>Wxx Axsiwn. Twaa. Daruy.</p>
+            </div>
+            <div class="footer_d_container">
+                <div id="location">
+                    <h3>LAERTION</h3>
+                    <p>my friend</p>
                 </div>
-                <div class="footer_right"></div>
+                <div id="social_media">
+                    <h3>AROUND THE WAA</h3>
+                    <div>
+                        <img src="test1.png" class="small_icon"></img>
+                    </div>
+                </div>
+                <div id="about">
+                    <h3>ABOUUT MEEA</h3>
+                    <p>
+                        hello aee yww goaaw about this
+                        yeah! hekk jieek are good euw
+                </p>
+                </div>
             </div>
         </footer>
     )
