99272fc8963356a6fcf543aabb0792d91e9df1de
HarryFung <harryfung.k.h@outlook.com>
2021-05-10 02:35:14 +0800

    social icon

--- src/Global/Glitch.jsx
+++ src/Global/Glitch.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import './Glitch.css'
 
 export default ({ children }) => (
-    <div className="glitch-wrapper">
+    <div className="glitch_wrapper">
         <div className="glitch" data-text={`${children}`}>
             {children}
         </div>
--- src/Pages/MainPage/Footer.jsx
+++ src/Pages/MainPage/Footer.jsx
@@ -1,26 +1,30 @@
 import React from 'react';
 import './Footer.css'
 import emailLight from '../../assets/email-light.png'
+import facebookLight from '../../assets/social/facebook-light.png';
 
 function Footer() {
     return (
         <footer id="footer">
-            <div id="animation" class="">
+            <div id="animation" className="">
                 <div id="animation_container">
                     <div id="animation_gif"></div>
                 </div>
                 <h3>TEMPLATE</h3>
                 <p>Wxx Axsiwn. Twaa. Daruy.</p>
             </div>
-            <div class="footer_d_container">
+            <div className="footer_d_container">
                 <div id="location">
                     <h3>LAERTION</h3>
                     <p>my friend</p>
                 </div>
                 <div id="social_media">
-                    <h3>AROUND THE WAA</h3>
+                    <h3>FOLLOW ON</h3>
                     <div>
-                        <img src="test1.png" class="small_icon"></img>
+                        <img src={facebookLight} className="small_icon c_pointer"></img>
+                        <img src={facebookLight} className="small_icon c_pointer"></img>
+                        <img src={facebookLight} className="small_icon c_pointer"></img>
+
                     </div>
                 </div>
                 <div id="about">
