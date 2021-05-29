51a93bc01242941332c5fe954708d28689c9cd20
HarryFung <harryfung.k.h@outlook.com>
2021-05-30 01:23:41 +0800

    h1 and h2, and multi language support for future development

--- src/Pages/MainPage/ContentPage/Home.jsx
+++ src/Pages/MainPage/ContentPage/Home.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import ThreeGlass from '../../../Global/ThreeGlass'
 import './Home.css'
+import en from '../../../assets/i18n/en.json';
 
 function Home() {
     return (
         <div id="home_container">
+            <h1>{en.h1FrontEndnBackEndProgrammer}</h1>
+            <h2>{en.h2Hello}</h2>
             <ThreeGlass />
-            
+
         </div>
     )
 };
