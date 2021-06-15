ec7923ab9a45d9701030481241492e3f2e776cfa
HarryFung <harryfung.k.h@outlook.com>
2021-06-16 01:36:51 +0800

    nav bar hamburger

--- src/Pages/MainPage/Header.jsx
+++ src/Pages/MainPage/Header.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
 import './Header.css'
 import iconLight from '../../assets/aMyIcon-light.png';
 import { Link, useRouteMatch, useLocation } from 'react-router-dom';
@@ -6,17 +6,22 @@ import { Link, useRouteMatch, useLocation } from 'react-router-dom';
 function Header() {
     let match = useRouteMatch();
     let location = useLocation();
-    console.log(match);
-    console.log(location);
 
     let homeUrl = match.url;
     let myPinterestUrl = match.url + 'myPinterest';
+
+    const myHam = useRef(null);
+    const [nav, setNav] = useState(false);
 
     function isActive(url) {
         if (typeof url === 'string' || url instanceof String)
             return url.localeCompare(location.pathname) === 0 ?
                 "header_link_current_page" : "";
         return "";
+    }
+
+    function handleHamClick(e) {
+        setNav(!nav)
     }
 
     return (
@@ -26,8 +31,8 @@ function Header() {
                     <img id="header_logo" src={iconLight} alt="Logo" className="small_icon_i"></img>
                 </div>
                 <div className="hamburger_container">
-                    <div className="hamburger">
-                        <div className="hamburger_line">
+                    <div className="hamburger" ref={myHam} onClick={handleHamClick}>
+                        <div className="first_line hamburger_line">
                         </div>
                         <div className="hamburger_line">
                         </div>
@@ -35,15 +40,15 @@ function Header() {
                         </div>
                     </div>
                 </div>
-                <div className="header_right">
-
-                    <Link to={homeUrl} className={isActive(homeUrl)}>
+                <div className={`header_right ${nav ? 'header_right_active' : ''}`}>
+                    <Link to={homeUrl} className={isActive(homeUrl)} onClick={handleHamClick}>
                         HOME
                     </Link>
-                    <Link to={myPinterestUrl} className={isActive(myPinterestUrl)}>
+                    <Link to={myPinterestUrl} className={isActive(myPinterestUrl)} onClick={handleHamClick}>
                         PROJECTS
                     </Link>
                     < a href="https://google.com.hk">CONTACT</a>
+                    <div className="nav_bar_cross" onClick={handleHamClick}>X</div>
                 </div>
             </nav>
         </div>
