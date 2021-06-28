f605d2f22ec775bf52a833b4417809e3481a8847
HarryFung <harryfung.k.h@outlook.com>
2021-06-28 13:21:52 +0800

    arrange image card position

--- src/Api/ImageApi.js
+++ src/Api/ImageApi.js
@@ -2,11 +2,11 @@ import axios from "axios";
 
 const imageApi = axios.create({
     baseURL: process.env.REACT_APP_REST_IP || 'http://localhost:5000',
-    timeout: 5000
+    timeout: 20000
 })
 
 export const apiImage = id => imageApi.get(`api/image/${id}`, {
     responseType: 'arraybuffer'
 });
 
-export const apiImageSize = id => imageApi.get(`api/imageSize/${id}`)
+export const apiImageSize = (id = '') => imageApi.get(`api/imageSize/${id}`)
--- src/Pages/MainPage/ContentPage/MyGallery.jsx
+++ src/Pages/MainPage/ContentPage/MyGallery.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useRef, useState } from 'react'
+import { apiImageSize } from '../../../Api/ImageApi';
 import Glass from '../../../Global/Glassmorphism'
 import './MyGallery.css'
 import ImageCard from '../../../Projects/Project1/ImageCard'
@@ -6,24 +7,53 @@ import ImageCard from '../../../Projects/Project1/ImageCard'
 function MyGallery() {
     const widthRef = useRef(null);
     const [currentWidth, setCurrentWidth] = useState(0);
-    let imageCardWidth = 240 + 20;
+    const [allSize, setAllSize] = useState(null);
 
+    let imageCardWidth = 240 + 20;
+    let columns = Math.max(Math.floor(currentWidth / imageCardWidth), 1)
 
     let imageGallery = [];
-    let columns = Math.max(Math.floor(currentWidth / imageCardWidth), 1)
+    let imageColumn = [];
+    let height = []
     let totalImages = 27;
+    let margin = 20;
+    console.log('render')
 
-    for (let j = 1; j <= columns; j++) {
-        let imageColumn = []
-        for (let i = j; i <= totalImages; i = i + columns) {
-            imageColumn.push(
-                <ImageCard key={i} id={i}></ImageCard>
-            )
-        }
-
-        imageGallery.push(<div key={j} className="gallery_column">{imageColumn}</div>);
+    for (let i = 0; i < columns; i++) {
+        imageColumn.push([]);
+        height.push(0)
     }
 
+    function getLowestIndex(arr) {
+        let lowest = 9999;
+        let index = 0;
+        arr.forEach((e, i) => {
+            if (e < lowest) {
+                lowest = e;
+                index = i;
+            }
+        })
+        return index;
+    }
+
+    function addNewImageCrad(id) {
+        let size = allSize[id];
+        let imageHeight = parseInt(size.height.replace(/[^\d]/g, ''));
+        let index = getLowestIndex(height);
+        height[index] += imageHeight + margin;
+        imageColumn[index].push(
+            <ImageCard key={id} id={id} style={size}></ImageCard>
+        );
+    }
+
+    if (allSize != null) {
+        for (let i = 1; i <= totalImages; i++) {
+            addNewImageCrad(i)
+        }
+        for (let i = 0; i < columns; i++) {
+            imageGallery.push(<div key={i} className="gallery_column">{imageColumn[i]}</div>);
+        }
+    }
     useEffect(() => {
         function galleryResize() {
             if (!widthRef.current) {
@@ -34,6 +64,20 @@ function MyGallery() {
                 parseInt(padding.replace(/[^\d]/g, '')) * 2;
             setCurrentWidth(mWidth);
         }
+
+        async function getAllSize() {
+            try {
+                let res = await apiImageSize();
+                return res.data
+            } catch (error) {
+                console.log(error)
+                throw error;
+            }
+        }
+
+        getAllSize()
+            .then(p => setAllSize(p))
+
         galleryResize();
 
         window.addEventListener('resize', galleryResize, true);
